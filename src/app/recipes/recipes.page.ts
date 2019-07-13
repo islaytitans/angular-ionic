c124f4ad601289caba59a59bbf1a0a262772c248
Jonathan Robbins <jonathan.robbins@rcn.org.uk>
2019-07-13 21:22:32 +0100

    use of ion-avatarand img to present elements from a list

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.mode';
 
 @Component({
   selector: 'app-recipes',
@@ -6,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
+recipes: Recipe[] = [
+  {
+    id: 'r1',
+    title: 'Burger',
+    imageUrl: 'https://media.gettyimages.com/photos/closeup-of-burger-picture-id763260055?s=612x612',
+    ingredients: ['Bun', 'Beef patty', 'Lettuce', 'Burger sauce']
+  },
+  {
+    id: 'r2',
+    title: 'Hotdog',
+    imageUrl: 'https://the-oasis-107415.square.site/uploads/1/2/5/3/125360156/s436006772643584401_p25_i1_w1200.jpeg',
+    ingredients: ['Bun', 'Pork sausage', 'Mustard']
+  }
+];
 
   constructor() { }
 
